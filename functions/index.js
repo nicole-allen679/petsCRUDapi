8170e0b37c1126cf87aea8bd0af9eb3aaf1dcc4d
nicole-allen679 <nicole.allen679@gmail.com>
2021-04-23 15:50:59 -0400

    everything works now

--- functions/index.js
+++ functions/index.js
@@ -15,24 +15,26 @@ function connectToFb() {
   }
 }
 
-app.get('/pets', (req, res) => {
+const allPets = (req, res) => {
   connectToFb()
   db.collection('pets')
     .get()
     .then((allPets) => {
       const arrayOfPets = []
       allPets.forEach((pet) => arrayOfPets.push(pet.data()))
+      res.send(arrayOfPets)
     })
-    res.send(allPets)
     .catch((err) => res.status(500).send('Error getting pets' + err.message))
-})
+}
+
+app.get('/pets', allPets)
 
 app.post('/pets', (req, res) => {
   connectToFb()
   const newPet = req.body
   db.collection('pets')
     .add(newPet)
-    .then(() => this.allPets(req, res))
+    .then(() => allPets(req, res))
     .catch((err) => res.status(500).send('Error adding pets' + err.message))
 })
 
@@ -40,7 +42,7 @@ app.patch('/pets', (req, res) => {
   connectToFb()
   const changePet = req.body
     .update(changePet)
-    .then(() => this.allPets(req, res))
+    .then(() => allPets(req, res))
     .catch((err) => res.status(500).send('Error changing pets' + err.message))
 })
 
@@ -50,7 +52,7 @@ app.delete('/pets', (req, res) => {
   db.collection('pets')
     .doc(petId)
     .delete()
-    .then(() => this.allPets(req, res))
+    .then(() => allPets(req, res))
     .catch((err) => res.status(500).send('Error deleting pet' + err.message))
 })
 
